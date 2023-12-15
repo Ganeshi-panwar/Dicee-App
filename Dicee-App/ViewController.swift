933914fe2488d661bdba32192a0522c84e60a3d7
Ganeshi <ganeshi8279@gmail.com>
2023-12-15 12:29:44 +0530

    Initial Commit

---
+++ Dicee-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee-App
+//
+//  Created by Yatharth Rawat on 15/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
