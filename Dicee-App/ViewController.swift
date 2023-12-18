7c61838fcc5e6bc25179e20c740778252cd39af9
Ganeshi <ganeshi8279@gmail.com>
2023-12-18 12:21:22 +0530

    first commit

--- Dicee-App/ViewController.swift
+++ Dicee-App/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var liftDiceNumber = 1
+    var rightDiceNumber = 5
+    
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceTwo")
+            
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        print("button got tapped")
+//        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceFour")
+//        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceFour")
+        
+        diceImageViewOne.image = [
+            UIImage(imageLiteralResourceName: "DiceOne"),
+            UIImage(imageLiteralResourceName: "DiceTwo"),
+            UIImage(imageLiteralResourceName: "DiceThree"),
+            UIImage(imageLiteralResourceName: "DiceFour"),
+            UIImage(imageLiteralResourceName: "DiceFive"),
+            UIImage(imageLiteralResourceName: "DiceSix")
+        ][liftDiceNumber]
+        liftDiceNumber += 1
+        diceImageViewTwo.image = [UIImage(imageLiteralResourceName: "DiceOne"),
+             UIImage(imageLiteralResourceName: "DiceTwo"),
+             UIImage(imageLiteralResourceName: "DiceThree"),
+             UIImage(imageLiteralResourceName: "DiceFour"),
+             UIImage(imageLiteralResourceName: "DiceFive"),
+             UIImage(imageLiteralResourceName: "DiceSix"),
+        ][rightDiceNumber]
+        rightDiceNumber -= 1
+        
+        
+    }
+    
 }
 
